6d5e2c7abf51b3f4c14e28007a7bbf4265725ae8
xuexin21 <lohxuexin1021@gmail.com>
2024-11-13 17:03:07 +0800

    Directory of the image was incorrect

--- src/main/java/com/example/demo/ShieldImage.java
+++ src/main/java/com/example/demo/ShieldImage.java
@@ -5,7 +5,7 @@ import javafx.scene.image.ImageView;
 
 public class ShieldImage extends ImageView {
 	
-	private static final String IMAGE_NAME = "/images/shield.png";
+	private static final String IMAGE_NAME = "/com/example/demo/images/shield.png";
 	private static final int SHIELD_SIZE = 200;
 	
 	public ShieldImage(double xPosition, double yPosition) {
