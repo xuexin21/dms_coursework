5dbe87d56cdd86b21da061ef4e07b1b8cc891003
xuexin21 <lohxuexin1021@gmail.com>
2024-11-13 17:00:48 +0800

    Adding {}

--- src/main/java/com/example/demo/LevelOne.java
+++ src/main/java/com/example/demo/LevelOne.java
@@ -18,8 +18,9 @@ public class LevelOne extends LevelParent {
 		if (userIsDestroyed()) {
 			loseGame();
 		}
-		else if (userHasReachedKillTarget())
+		else if (userHasReachedKillTarget()) {
 			goToNextLevel(NEXT_LEVEL);
+		}
 	}
 
 	@Override
