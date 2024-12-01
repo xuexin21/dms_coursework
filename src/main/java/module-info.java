e4b9ce8ccfcdc619f777a475f109c22485dba467
xuexin21 <lohxuexin1021@gmail.com>
2024-12-01 16:10:59 +0800

    update the module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,17 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens com.example.demo.controller to javafx.fxml;
+    opens com.example.demo.level to javafx.fxml;
+    opens com.example.demo.level.levelview to javafx.fxml;
+    opens com.example.demo.model to javafx.fxml;
+    opens com.example.demo.projectile to javafx.fxml;
+    opens com.example.demo.view to javafx.fxml;
 
-    opens com.example.demo to javafx.fxml;
     exports com.example.demo.controller;
+    exports com.example.demo.level;
+    exports com.example.demo.level.levelview;
+    exports com.example.demo.model;
+    exports com.example.demo.projectile;
+    exports com.example.demo.view;
 }
