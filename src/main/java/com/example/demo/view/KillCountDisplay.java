96b66e7952ced6ccb151533f996621665bc49dad
xuexin21 <lohxuexin1021@gmail.com>
2024-12-07 18:20:04 +0800

    create a KillCountDisplay.java to display the number of kills

---
+++ src/main/java/com/example/demo/view/KillCountDisplay.java
@@ -0,0 +1,24 @@
+package com.example.demo.view;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+public class KillCountDisplay extends StackPane {
+    private static final int xPosition = 1080;
+    private static final int yPosition = 10;
+    private final int initialKillCount;
+    private final Label killCountText;
+
+    public KillCountDisplay(int initialKillCount) {
+        this.initialKillCount = initialKillCount;
+        this.killCountText = new Label("Kills: 0/" + initialKillCount);
+        this.killCountText.setStyle("-fx-font-size: 18; -fx-text-fill: black; -fx-font-weight: bold;");
+        this.setLayoutX(xPosition);
+        this.setLayoutY(yPosition);
+        this.getChildren().add(killCountText);
+    }
+
+    public void updateKillCount(int killCount) {
+        this.killCountText.setText("Kills: " + killCount + "/" + initialKillCount);
+    }
+}
