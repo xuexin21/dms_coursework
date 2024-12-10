aa5045893af394895c7cfdd70fc7fb51aad0a99b
xuexin21 <lohxuexin1021@gmail.com>
2024-12-10 23:44:08 +0800

    created secondBoss

---
+++ src/main/java/com/example/demo/model/SecondBoss.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public class SecondBoss extends Boss {
+
+    private static final int INITIAL_HEALTH = 100;
+
+    public SecondBoss() {
+        super(INITIAL_HEALTH);
+    }
+}
